1c4422a9b2a3f3a43faf70fafae2d3236ea76173
AntaresT <luizeduardovbe@gmail.com>
2022-04-06 15:16:27 -0300

    feature: code adjustment and file cleaning

--- src/Components/Sweeper/index.js
+++ src/Components/Sweeper/index.js
@@ -12,7 +12,7 @@ function Sweeper () {
   const [attractionInfo, setAttractionInfo] = useState({});
 
   return(
-    <Container position={itemToSearch===''? true : false}>
+    <Container position={videosList===[]? true : false}>
       <SearchBox 
         searchItem={itemToSearch} 
         onChange={ (e) => {setItemToSearch(e.target.value)} }
